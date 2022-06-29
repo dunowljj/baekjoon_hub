aea75304063e09a3df4bda20f719e2604905e2df
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-06-29 18:47:50 +0900

    Update N과 M （1）.java
    update

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static boolean[] visit;
-    static StringBuilder sb = new StringBuilder();
-
+    static int N;
+    static int M;
+    static StringBuilder sb;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(N,M,0);
-        System.out.println(sb);
+        visited = new boolean[N + 1];
+
+        dfs(0, "");
+
+        bw.write(sb.toString().trim());
+        bw.flush();
+        bw.close();
     }
-    public static void dfs(int N, int M, int depth){
-        if(depth == M){
-            for(int i=0; i<arr.length; i++) {
-                sb.append(arr[i]).append(' ');
-            }
-            sb.append('\n');
+
+    static void dfs(int depth, String answer) {
+        if (depth == M) {
+            sb.append(answer+"\n");
             return;
         }
 
-        for(int i=0; i<N; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
-                visit[i] = false;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(depth + 1, answer + i + " ");
+                visited[i] = false;
             }
         }
-
     }
 }
+/*
+출력 : 1~N 까지 수에서 M개 고른 수열
+중복 수열 x
+-> 순서만 바꾼것은 중복으로 치지 않는다.
+같은 수 한번만 선택 가능.
+공백 구분 출력
+
+1~N까지 DFS, 방문 여부 체크해서 겹치지 않게해서
+ */
