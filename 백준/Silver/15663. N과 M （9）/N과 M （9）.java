9ddbfb29f47296fa248739f113fe26124129e468
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-07-02 08:32:20 +0900

    [Silver II] Title: N과 M (9), Time: 208 ms, Memory: 21208 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,62 +1,68 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
     static int[] inputNums;
+    static int[] arr;
     static boolean[] visited;
-    static StringBuilder sb;
-    static Set<String> set;
-    public static void main(String[] args) throws IOException {
+    static int M;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sb = new StringBuilder();
-        set = new LinkedHashSet<>();
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[M]; //수열 저장
-        visited = new boolean[N]; // 방문 여부
-        //입력된 숫자들 저장
+
         inputNums = new int[N];
+        visited = new boolean[N];
+        arr = new int[M];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i<N; i++)
             inputNums[i] = Integer.parseInt(st.nextToken());
-        }
+
         Arrays.sort(inputNums);
 
         dfs(0);
-        for (String perm : set) {
-            sb.append(perm);
-        }
-
 
         bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
     }
+
     static void dfs(int depth) {
         if (depth == M) {
             for (int num : arr) {
                 sb.append(num).append(" ");
             }
             sb.append("\n");
-            set.add(sb.toString());
-            sb.setLength(0);
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = inputNums[i];
-                dfs(depth + 1);
-                visited[i] = false;
+        else{
+            int before = 0;
+            for(int i = 0; i<N; i++){
+                if(visited[i])
+                    continue;
+
+                if(before != inputNums[i]){
+                    visited[i] = true;
+                    arr[depth] = inputNums[i];
+                    before = inputNums[i];
+                    dfs(depth+1);
+                    visited[i] = false;
+                }
             }
         }
     }
 
 }
+/*
+dfs를 재귀해서 들어갈 땐 before가 0으로 초기화되고, for문을 순회할때는 이전값이 저장된다.
+-> 같은 수가 입력되었을때, 해당 자릿수에 똑같은 수가 또 들어올 수 없게 하는 장치
+ */
