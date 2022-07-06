2c6f03e72b513b60a378b6d6f564cfdee17b158e
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-07-07 08:38:57 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -10,7 +10,6 @@ public class Main {
     static boolean[] isVowel;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        final char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -32,25 +31,15 @@ public class Main {
 
         Arrays.sort(chs);
 
-        // isVowel에 모음 미리 체크해두기
-        for (int i = 0; i < chs.length; i++) {
-            for (int j = 0; j < vowels.length; j++) {
-                if (chs[i] == vowels[j]) {
-                    isVowel[i] = true;
-                }
-            }
-        }
-
-
-        dfs(0, 0,0 , 0);
+        dfs(0, 0);
 
         bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
     }
-    static void dfs(int depth, int countV, int countC, int start) {
+    private static void dfs(int depth, int start) {
         if (depth == L) {
-            if (countC >= 2 && countV >= 1) {
+            if (isValid()) {
                 sb.append(password).append("\n");
             }
             return;
@@ -59,30 +48,37 @@ public class Main {
         for (int i = start; i < chs.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                if (isVowel[i])  {
-                    countV++;
-                } else {
-                    countC++;
-                }
+
                 password[depth] = chs[i];
-                dfs(depth + 1, countV, countC, i + 1);
+                dfs(depth + 1, i + 1);
 
                 visited[i] = false;
-                if (isVowel[i]) {
-                    countV--;
-                } else {
-                    countC--;
-                }
             }
         }
+    }
+
+    private static boolean isValid() {
+        int vowel = 0;
+        int consonant = 0;
+        for (char ch : password) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowel++;
+            } else {
+                consonant++;
+            }
+        }
+        return vowel >= 1 && consonant >= 2;
     }
 }
 /*
 L 암호 길이, C 주어진 문자
 3<=L<=C<=15
 증가하는 순서 o
-최소 1개 모음, 최소 2개 자음 -> 최소! 1개 이상, 2개 이상 있어야 함
-1) 모음,자음 개수를 세고 마지막에 처리 o
 
 미리 오름차순 정렬, 중복 미리 제거 o
+
+최소 1개 모음, 최소 2개 자음 -> 최소! 1개 이상, 2개 이상 있어야 함
+->모음,자음 개수를 마지막에 검증 o   
+
+
  */
