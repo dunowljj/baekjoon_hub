a8c7c5aff89d0351b9d2850e7838689273c69907
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-07-18 08:45:04 +0900

    [Silver II] Title: 섬의 개수, Time: 168 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map;
-    static boolean[][] visited;
+    static boolean[][] needSearch;
     static int[] idxMapper = {-1, 0, 1};
 
     static int h;
@@ -13,7 +12,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        map = new int[50][50];
+        // 최대 50, idx편의를 위해 +2하려 배열의 둘레에 0을 한 줄 생성
+        needSearch = new boolean[52][52];
         StringTokenizer st;
         boolean noIsland = true;
 
@@ -26,19 +26,22 @@ public class Main {
             // 0 0 나오면 탈출
             if (w == 0 && h == 0) break;
 
-            // map에 값 입력
-            for (int i = 0; i < h; i++) {
+            /*
+            boolean으로 변환 1 -> true(방문x), 0 -> false(방문o)
+            boolean은 false가 초깃값인데, 초기화시 둘레 값은 false가 된다.
+            둘레는 방문해서는 안되므로, false인 값을 방문하지 않도록 설정해야 한다.
+            논리적으로; 탐색이 필요하다(true) -> 방문(dfs) / 탐색이 필요없다(false) -> 방문 x
+             */
+            for (int i = 1; i <= h; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                for (int j = 1; j <= w; j++) {
+                    needSearch[i][j] = Integer.parseInt(st.nextToken()) == 1;
                 }
             }
-            // while 순회마다 초기화
-            visited = new boolean[h][w];
 
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
+            for (int i = 1; i <= h; i++) {
+                for (int j = 1; j <= w; j++) {
+                    if (needSearch[i][j]) {
                         dfs(i, j);
                         islandCount++;
                         noIsland = false;
@@ -59,17 +62,15 @@ public class Main {
         bw.close();
     }
     static void dfs(int x, int y) {
-        visited[x][y] = true;
+        needSearch[x][y] = false;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 int nx = x + idxMapper[i];
                 int ny = y + idxMapper[j];
 
-                if (0 <= nx && nx < h && 0 <= ny && ny < w) {
-                    if (!visited[nx][ny] && map[nx][ny] == 1) {
-                        dfs(nx, ny);
-                    }
+                if (needSearch[nx][ny]) {
+                    dfs(nx, ny);
                 }
             }
         }
@@ -80,8 +81,6 @@ public class Main {
 가로, 세로, 대각선까지 이동 가능
 너비, 높이 <= 50
 테스트케이스의 개수 모름. 여러개.
-
-for문 순회 -> 섬 발견 시 dfs로 주변 모두 탐색 -> 발견한 섬 방문체크 -> 섬 개수를 세서 append후 초기화
 
 섬이 0개일때 -> 아예 값이 생기지 않음
 덩어리 개수를 세야한다.
