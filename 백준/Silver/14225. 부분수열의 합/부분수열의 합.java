e7056c35d73b191c869c8599dd57063f3afe93fc
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-05 15:03:41 +0900

    [Silver I] Title: 부분수열의 합, Time: 168 ms, Memory: 16676 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] subSum;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        visited = new boolean[N];
+        subSum = new boolean[100_000 * 20 + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0, 0,0, arr);
+
+        for (int i = 1; i < subSum.length; i++) {
+            if (!subSum[i]) {
+                bw.write((i)+"");
+                break;
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+    static void dfs(int start, int depth, int sum, int[] arr) {
+        if (!subSum[sum]) {
+            subSum[sum] = true;
+        }
+        if (depth == arr.length) {
+            return;
+        }
+
+        for (int i = start; i < arr.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i + 1, depth + 1, sum + arr[i], arr);
+                visited[i] = false;
+            }
+        }
+    }
+}
+/*
+1~20크기 수열
+수열의 수들은 100_000이하
+
+생각
+20개 -> 2의 20승가지 -> 2초 -> 완전탐색
+
+작은 순으로 합을 정렬, 1부터 2의 20승까지 탐색하면서 합이 일치하지 않는 것 탐색
+
+ */
