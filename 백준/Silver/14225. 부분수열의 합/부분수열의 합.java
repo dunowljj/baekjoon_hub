103c520d2f640f3642f0efbc0ec806a855807ac5
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-05 15:17:13 +0900

    [Silver I] Title: 부분수열의 합, Time: 148 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -4,14 +4,12 @@ import java.util.StringTokenizer;
 
 public class Main {
     static boolean[] subSum;
-    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        visited = new boolean[N];
         subSum = new boolean[100_000 * 20 + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,7 +18,7 @@ public class Main {
         }
 
         Arrays.sort(arr);
-        dfs(0, 0, 0, arr);
+        dfs(0,0, arr);
 
         for (int i = 1; i < subSum.length; i++) {
             if (!subSum[i]) {
@@ -31,23 +29,22 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    static void dfs(int idx, int depth, int sum, int[] arr) {
-        if (depth == arr.length) {
+    static void dfs(int idx, int sum, int[] arr) {
+        if (idx == arr.length) {
             subSum[sum] = true;
             return;
         }
 
-        dfs(idx + 1, depth + 1, sum + arr[idx], arr);
-        dfs(idx + 1, depth + 1, sum, arr);
+        dfs(idx + 1, sum + arr[idx], arr);
+        dfs(idx + 1, sum, arr);
 
         }
     }
 /*
 1~20크기 수열
-수열의 수들은 100_000이하
+수열의 수들은 100_000이하 -> 10만 * 20이 합의 최대
 
-생각
-20개 -> 2의 20승가지 -> 2초 -> 완전탐색
+20개 -> 2의 20제곱개 탐색 -> 2초 제한 (매우 충분) -> 완전탐색
 
 작은 순으로 합을 정렬, 1부터 2의 20승까지 탐색하면서 합이 일치하지 않는 것 탐색
 
