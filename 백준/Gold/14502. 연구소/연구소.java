dd7c5ffb988accc0674b75b9f39e50a3df839f27
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-16 16:26:31 +0900

    [Gold IV] Title: 연구소, Time: 384 ms, Memory: 119456 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,35 +1,40 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Point1 {
+class Coordinate {
     int x;
     int y;
 
-    public Point1(int x, int y) {
+    public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
 
 public class Main {
-    public static final int ADDED_WALL = 3;
+//    public static final int ADDED_WALL = 3;
     static int N;
     static int M;
     static int[][] board;
     static int answer = 0;
-    static int wallCount = 0;
+//    static int wallCount = 0;
+    static int infectCount = 0;
+//    static int initialInfected = 0;
+    static int[][] mapper = {{1, -1, 0, 0}, {0, 0, -1, 1}};
+    static List<Coordinate> infected = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final int ADDED_WALL = 3;
+        int initialInfected = 0;
+        int wallCount = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         board = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -39,25 +44,30 @@ public class Main {
                 if (board[i][j] == 1) {
                     wallCount++;
                 }
+
+                if (board[i][j] == 2) {
+                    initialInfected++;
+                    infected.add(new Coordinate(i,j));
+                }
             }
         }
 
-        safeZoneDFS(0, 0, 0);
+        setWallDFS(0, 0, 0);
 
-        bw.write(answer+"");
+        bw.write((answer - initialInfected - wallCount - ADDED_WALL) + "");
         bw.flush();
         bw.close();
     }
 
     // 벽 3개 dfs로 지정. 각 경우의 수마다 bfs
-    private static void safeZoneDFS(int depth, int row, int col) {
+    private static void setWallDFS(int depth, int row, int col) {
         if (depth == 3) {
             answer = Math.max(answer, bfs());
             return;
         }
 
         if (col == M) {
-            safeZoneDFS(depth, row + 1, 0);
+            setWallDFS(depth, row + 1, 0);
             return;
         }
 
@@ -67,41 +77,29 @@ public class Main {
 
         if (board[row][col] == 0) {
             board[row][col] = 1;
-            safeZoneDFS(depth + 1, row, col + 1);
+            setWallDFS(depth + 1, row, col + 1);
             board[row][col] = 0;
         }
 
         //0 인경우, 아닌 경우 모두 그냥 넘어가는 경우도 있어야
-        safeZoneDFS(depth, row, col + 1);
+        setWallDFS(depth, row, col + 1);
     }
 
     private static int bfs() {
-        Queue<Point1> queue = new LinkedList<>();
-        boolean[][] infected = new boolean[N][M];
+        Queue<Coordinate> queue = new LinkedList<>();
 
-        int[][] mapper = {{1, -1, 0, 0}, {0, 0, -1, 1}};
-
-        int infectCount = 0;
-        // 감염 위치 찾아서 넣기
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (board[i][j] == 2) {
-                    infected[i][j] = true;
-                    infectCount++;
-                    queue.add(new Point1(i, j));
-                }
-            }
+        // 감염 위치만 큐에 넣기
+        for (Coordinate coordinate : infected) {
+            queue.add(coordinate);
         }
 
-
+        infectCount = 0;
         while (!queue.isEmpty()) {
-            Point1 point = queue.poll();
+            Coordinate point = queue.poll();
             int x = point.x;
             int y = point.y;
 
-
             for (int i = 0; i < 4; i++) {
-
                 int nx = x + mapper[0][i];
                 int ny = y + mapper[1][i];
 
@@ -109,25 +107,41 @@ public class Main {
                     continue;
                 }
 
-                // 현재 위치 감염 -> 애초에 감염된 것만 넣음 -> 처음 넣는 것은 감염이 아닐수도 있음
-                // 주변이 벽이 아니고 비감염이면 감염처리
-                if (board[nx][ny] != 1 && !infected[nx][ny]) {
-                    infected[nx][ny] = true;
+                // 0인 경우만 감염된다.
+                if (board[nx][ny] == 0) {
+                    board[nx][ny] = -1;
                     infectCount++;
-                    queue.add(new Point1(nx, ny));
+                    queue.add(new Coordinate(nx, ny));
                 }
             }
         }
 
-        /*int count = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (!infected[i][j] && board[i][j] == 0) {
-                    count++;
+                if (board[i][j] == -1) {
+                    board[i][j] = 0;
                 }
             }
-        }*/
-        return (N * M) - wallCount - infectCount - ADDED_WALL;
+        }
+
+        return (N * M) - infectCount;
     }
 
 }
+/*
+## 조건
+0 빈칸 , 1 벽, 2 바이러스
+안전영역 -> 벽과 바이러스 제외한 0 부분
+2의 개수는 2보다 크거나 같다.
+벽은 꼭 3개를 세워야 한다.
+
+## 시간 2초
+3 <= N,M <= 8  ; 최대 8*8 -> 64칸
+벽 3개 선택 -> N6제곱
+--> 2의 18제곱  * bfs(N제곱 * 주변탐색) : 모두 탐색해도 충분하다.
+
+## 풀이
+### 큐에 넣는 조건?
+감염된 위치만 큐에 먼저 넣어놓고, 주변에 0을 감염시키는 방식으로 구현한다. 0,1,2와 겹치지 않는 숫자값을 입력 배열에 집어넣고
+연산 후 다시 초기화시켜주는 방식을 사용하면 메모리와 시간을 절약할 수 있다.
+ */
