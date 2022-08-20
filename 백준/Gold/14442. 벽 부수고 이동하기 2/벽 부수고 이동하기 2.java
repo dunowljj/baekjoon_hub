361150945789ef3fea0316b0b72eab8f88f3f278
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-20 09:09:45 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1268 ms, Memory: 315540 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -53,9 +53,17 @@ public class Main {
         queue.add(new Point3(0, 0, K, 1));
 
         int[][] mapper = {{1, -1, 0, 0}, {0, 0, 1, -1}};
-        boolean[][][] visited = new boolean[K + 1][N][M];
-        visited[K][0][0] = true;
-        visited[K - 1][0][0] = true;
+        int[][] visited = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!isWall[i][j]) {
+                    visited[i][j] = -1;
+                }
+            }
+        }
+        //방문여부를 남은 부술수있는 벽의 개수로 갱신
+        visited[0][0] = K;
 
         while (!queue.isEmpty()) {
             Point3 point = queue.poll();
@@ -75,27 +83,17 @@ public class Main {
                 }
 
 
-                if (!visited[k][nx][ny]) {
-                    visited[k][nx][ny] = true;
+                // 현재 부술 수 있는 벽의 개수가 더 큰 경우만
+                if (visited[nx][ny] < k) {
+                    visited[nx][ny] = k;
 
                     if (isWall[nx][ny]) {
                         k--;
                     }
-
                     queue.add(new Point3(nx, ny, k, point.count + 1));
                 }
 
-               /* if (isWall[nx][ny] && !visited[k][nx][ny]) {
-                    visited[k][nx][ny] = true;
-                    queue.add(new Point3(nx, ny, k - 1));
-                }
-
-                if (!isWall[nx][ny] && !visited[k][nx][ny]) {
-                    visited[k][nx][ny] = true;
-                    queue.add(new Point3(nx, ny, k));
-                }*/
             }
-
 
         }
         return -1;
@@ -104,7 +102,11 @@ public class Main {
 }
 /*
 부술수있는 벽의 개수가 여러개
-더 많이 부수고 온것과 더 적게 부수고 온것 모두 방문체크를하면 배열을 10배로 만들어야한다.
-부순횟수를 비교해서 더 적게 부순 경우를 하기엔 애매하다. 최단 거리가 무엇인지 알 수 없기떄문이다.
 
+bfs를 사용하면 특정 위치(노드)에 도착하는 시점은 같다. 즉, 벽을 부순 횟수를 비교하여 효율을 높일 수 있다.
+같은 위치에 도달했을때 걸린 횟수는 같으므로, 벽을 부순 횟수가 적은 것만 선택하면 되기 때문이다.
+
+벽인 경우를 확인하기 위한 boolean[][] isWall 배열 + 방문배열 int[][] visited
+isWall로 벽 여부를 확인하고, 방문배열에 부순 벽을 저장하면서 진행
+방문배열에 부순 횟수 0과, 방문하지 않은곳 0을 구분하기 위해 벽이 없는 곳을 -1 처리
  */
