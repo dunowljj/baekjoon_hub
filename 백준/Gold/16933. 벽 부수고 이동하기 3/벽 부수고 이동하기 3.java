d6b8140fb08646090bb88e2c1ed107b99d9593fc
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-22 08:00:24 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1524 ms, Memory: 311332 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -8,16 +8,12 @@ class SnapShot {
     int y;
     int distance;
     int canBreak;
-    int stayBit;
-    boolean isNight;
 
-    public SnapShot(int x, int y, int distance, int canBreak, int stayBit, boolean isNight) {
+    public SnapShot(int x, int y, int distance, int canBreak) {
         this.x = x;
         this.y = y;
         this.distance = distance;
         this.canBreak = canBreak;
-        this.stayBit = stayBit;
-        this.isNight = isNight;
     }
 
 }
@@ -36,14 +32,13 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         board = new char[N][M];
-        int[][][] visited = new int[2][N][M];
+        int[][] visited = new int[N][M];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
                 board[i][j] = input.charAt(j);
 
-                visited[0][i][j] = -1;
-                visited[1][i][j] = -1;
+                visited[i][j] = -1;
             }
         }
 
@@ -52,63 +47,59 @@ public class Main {
         bw.close();
     }
 
-    static int bfs(int K, int[][][] visited) {
+    static int bfs(int K, int[][] visited) {
         Queue<SnapShot> queue = new LinkedList();
-        queue.add(new SnapShot(0, 0, 1, K, 0,false));
+        queue.add(new SnapShot(0, 0, 1, K));
 
         int[][] mapper = {{1, -1, 0, 0}, {0, 0, 1, -1}};
-        visited[0][0][0] = K + 1;
-        visited[1][0][0] = K + 1;
+        visited[0][0] = K + 1;
 
         while (!queue.isEmpty()) {
             SnapShot snapShot = queue.poll();
 
             int distance = snapShot.distance;
-            int canBreak = snapShot.canBreak;
-            boolean isNight = snapShot.isNight;
 
             // 도착시 종료
             if (snapShot.x == N - 1 && snapShot.y == M - 1) {
                 return distance;
             }
 
-            // 밤일때 제자리에서 기다리는 경우 추가
-            if (isNight) {
-                queue.add(new SnapShot(snapShot.x, snapShot.y, distance + 1, canBreak, 1,false));
-            }
+
 
             for (int i = 0; i < 4; i++) {
                 int nx = snapShot.x + mapper[0][i];
                 int ny = snapShot.y + mapper[1][i];
-                canBreak = snapShot.canBreak;
-                int stayBit = snapShot.stayBit;
+                int canBreak = snapShot.canBreak;
+
+
+                // 인덱스 초과, 기다려도 부술수 없는 경우
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M || (board[nx][ny] == '1' && canBreak == 0)) {
                     continue;
                 }
-                
-                // 현재 문제는 사방이 막힌 경우에 제자리에서 기다리는 경우가 문제인 것 같다.
-                // 큐에 기다리는 경우를 넣어놓고, 다음 위치들을 탐색하는 구조이다.
-                // 밤의 경우 continue 한 후 나중에 처리하는데, 방문체크는 이미 해놓기떄문에, 기다렸다가 출발하는 경우를 나중에 처리할때 지나갈 수 없다.
-                // 그렇다고 한번에 거리를 2개씩 전진시키는 방법을 쓰면, 최단경로를 구하기 위해 bfs를 한 이유가 없어진다.
-                // stayBit을 추가해서 방문배열을 분리했다.
 
-                if (visited[stayBit][nx][ny] < canBreak) {
-                    visited[stayBit][nx][ny] = canBreak;
+                // 한턴 기다린 작업이 부술수 있는 경우가 더 많을 수 있나?
 
-                    // stayBit == 1 -> 기다렸다가 온 경우, 주변이 벽이 아닌 경우는 탐색할 필요가 없다.
-                    if (stayBit == 1 && board[nx][ny] == '0') continue;
+                if (visited[nx][ny] >= canBreak) continue;
 
-                    // 벽이있으면 부술수 있는 횟수 차감
-                    if (board[nx][ny] == '1') {
-                        // 밤이면 못지나감
-                        if (isNight) {
-                            continue;
-                        }
-                        canBreak--;
+                if (board[nx][ny] == '1') {
+                    // 부순다고 가정했을때보다 많은 횟수가 남아있다면, 다음번 탐색할 필요가 없다.
+                    if (visited[nx][ny] >= canBreak - 1) continue;
+
+                    // 기다리면 부술 수 있는 경우 = 밤일때 제자리에서 기다리는 경우 -> 현재 정보 넣고 바로 다음 탐색
+                    if (distance % 2 == 0) {
+                        queue.add(new SnapShot(snapShot.x, snapShot.y, distance + 1, canBreak));
+                        continue;
                     }
 
-                    queue.add(new SnapShot(nx, ny, distance + 1, canBreak, 0, !isNight));
+                    // 당장 부수는 경우
+                    else {
+                        canBreak--;
+                    }
                 }
+
+                // 0인경우, 당장 부순 경우 추가
+                visited[nx][ny] = canBreak;
+                queue.add(new SnapShot(nx, ny, distance + 1, canBreak));
             }
         }
         return -1;
@@ -119,4 +110,12 @@ public class Main {
 밤낮을 boolean으로 표현
 
 밤이면 제자리에서 거리만 더하는 경우 추가
+
+
+다른 풀이
+부순개수를 세면서 탐색
+1) 부순개수 > K
+
+방문체크 구조를 바꾸면 된다.
+거리를 % 연산한다.
  */
