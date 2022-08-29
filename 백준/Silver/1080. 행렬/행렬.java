a8d1bd75a0376472d2ad48e235a8c6aaecd69313
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-08-30 08:43:23 +0900

    [Silver I] Title: 행렬, Time: 124 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] start;
-    static char[][] dest;
+    static int[][] start;
+    static int[][] dest;
     static int count;
     static int N;
     static int M;
@@ -16,19 +16,19 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
 
-        start = new char[N][M];
+        start = new int[N][M];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                start[i][j] = input.charAt(j);
+                start[i][j] = input.charAt(j) - '0';
             }
         }
 
-        dest = new char[N][M];
+        dest = new int[N][M];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                dest[i][j] = input.charAt(j);
+                dest[i][j] = input.charAt(j) - '0';
             }
         }
 
@@ -41,7 +41,7 @@ public class Main {
         bw.close();
     }
 
-    private static boolean isSame(char[][] start, char[][] dest) {
+    private static boolean isSame(int[][] start, int[][] dest) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (start[i][j] != dest[i][j]) {
@@ -65,11 +65,7 @@ public class Main {
     private static void changeSubMatrix(int i, int j) {
         for (int k = i; k < i + 3; k++) {
             for (int l = j; l < j + 3; l++) {
-                if (start[k][l] == '0') {
-                    start[k][l] = '1';
-                } else {
-                    start[k][l] = '0';
-                }
+                start[k][l] ^= 1;
             }
         }
     }
@@ -78,5 +74,9 @@ public class Main {
 N,M <= 50 자연수
 
 3*3의 부분 행렬을 통째로 뒤집어야함. 탐색이 아닌 그리디?
-맨 왼쪽위의 요소를 기준으로 값이 다르면 부분행렬을 뒤집는다. 중복되지 않게 뒤집는게 관건
+
+다른코드 : 모든 인덱스를 탐색하면서, 부분수열을 바꿀수 없는 부분까지 탐색한다. 인덱스가 넘어감을 따로 처리해준다.
+바꾸려는 해당요소가 다른데 바꿀수 없는 경우 바로 종료하는 조건을 추가한다.
+
+int가 빠른가? 싶어서 토글연산을 넣어서 만들어봄
  */
