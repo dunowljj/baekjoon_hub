15dc9f0d606f4dda7a7a1e7cf08e15fd5347f33d
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-20 14:33:01 +0900

    Update 양궁대회.java

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -3,18 +3,13 @@ class Solution {
     int max = Integer.MIN_VALUE;
     int[] answer = new int[11];
     boolean[] visited = new boolean[11];
+    
     public int[] solution(int n, int[] info) {
         int[] result = new int[11];
         
-        // int apacheSum = 0;
-        // for (int i = 0; i < info.length; i++) {
-        //     if (info[i] != 0) apacheSum += 10 - i;
-        // }
-        // // 점수를 합해놓고 빼니깐, 겹치는 부분에는 점수를 *2로 빼야하는데 그게 안된다.
-        
         dfs(0, n, info, answer, 10);
         
-        // 차이의 최솟값이 0보다 크거나 같으면 어파치 승리
+        // (라이언점수 - 어파치점수)의 최댓값이 0보다 작거나 같으면 어파치 승리
         if (max <= 0) {
             return new int[]{-1};
         } else {
@@ -28,7 +23,6 @@ class Solution {
             if (max < score) {
                 max = score;
                 answer = result.clone();
-                
             }
             return;
         }
@@ -49,15 +43,12 @@ class Solution {
         }
     }
     
-    
-    
-    
     private int getScoreDiff(int[] ach, int[] lion) {
         int achScore = 0;
         int lionScore = 0;
         
         for (int i = 0; i < ach.length; i++) {
-            if (ach[i] == 0 && lion[i] == 0) continue;
+            if (ach[i] == 0 && lion[i] == 0) continue; //둘다 0인 경우는 점수 획득이 없으므로 계산하지 않는다.
             
             if (ach[i] >= lion[i]) {
                 achScore += (10 - i);
@@ -72,16 +63,12 @@ class Solution {
 /*
 화살을 쏠수있는 총 경우의 수 최대 = 10^11
 완전탐색 후 최댓값 구하기
+지는 경우 뿐이라면 길이 1 [-1] return
 
-해가 여러가지 -> 가장 낮은 점수를 더 많이 맞힌 경우를 return? 아래부터 탐색 -> 화살이 남는데 점수를 더 얻을 수 없는경우는? -> 뒤부터 탐색해서 조건없이 탐색해보기
+해가 여러가지 -> 가장 낮은 점수를 더 많이 맞힌 경우를 return 뒤부터 탐색 -> 뒤부터 탐색해서 조건없이 탐색해보기
+뒤부터 완전탐색을 하니 당연히 시간초과가 난다.
 
-뒤부터 완전탐색을 하면, 시간초과가 난다. -> 근데 그냥 실패도 몇개 껴있다.
-화살을 아파치의 화살 개수를 넘을때까지만 과녁을 맞추도록 하면 어떨까? 문제는 남는 화살의 처리이다.
+해결 : 화살을 아파치의 화살 개수를 넘을때까지만 과녁을 맞추도록 하기. 화살의 개수는 정해져있으므로, 최대 점수를 구하고도 화살이 남을 일은 없다.
 
-1~10까지 방문처리를 한후, 화살 개수가 넘어가면 true 처리, 모두 true라면 제일 작은곳에 화살 쏘기 -> 애초에 모두 true이면 화살이 남지 않는다.
-
-
-뭘해도 지면 길이 1 [-1] return
-
-
+1~10까지 방문처리를 한후, 화살 개수가 넘어가면 탐색 멈춤. 점수가 제일 작은곳부터 화살 쏘기
 */
