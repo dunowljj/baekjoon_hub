4ae1f2938a415567f9f07ebb77e0c0511a85b099
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-05-02 14:04:11 +0900

    [level 2] Title: 전화번호 목록, Time: 222.24 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,6 +2,25 @@ import java.util.stream.*;
 import java.util.*;
 
 class Solution {
+     // hash
+     public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String pNum : phone_book) {
+            map.put(pNum, Integer.MAX_VALUE);
+        }
+        
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                String subNum = phone_book[i].substring(0,j);
+                if (map.containsKey(subNum)) return false;
+            }
+        }
+            
+        return true;
+    }
+    
+    /*
+    // sort
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
@@ -10,5 +29,5 @@ class Solution {
         }
             
         return true;
-    }
+    }*/
 }
