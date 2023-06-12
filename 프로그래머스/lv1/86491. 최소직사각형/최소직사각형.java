7942af69f9fd79f82baadcdc4c15527e5515f5c6
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-06-13 08:03:31 +0900

    [level 1] Title: 최소직사각형, Time: 5.17 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -3,39 +3,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = Integer.MAX_VALUE;
         
         for (int i = 0; i < sizes.length; i++) {
             Arrays.sort(sizes[i]);
+        }    
+        
+        int widthMax = 0;
+        int heightMax = 0;
+        
+        for (int i = 0; i < sizes.length; i++) {
+            widthMax = Math.max(sizes[i][0], widthMax);
+            heightMax = Math.max(sizes[i][1], heightMax);
         }
-        
-        answer = Math.min(answer,getMax(sizes));    
 
-        
-        return answer;
+        return widthMax * heightMax;
     }
     
    
     
-    public int getMax(int[][] sizes) {
-        int maxW = 0;
-        int maxH = 0;
-
-        for (int i = 0; i < sizes.length; i++) {
-            maxW = Math.max(maxW, sizes[i][0]);
-            maxH = Math.max(maxH, sizes[i][1]);
-        }
-        
-        return maxW * maxH;
-    }
+   
 }
-
-/*
-명함의 개수 : 1이상 10_000이하
-w,h <= 1_000 자연수
-뒤집는 모든 경우의 수? -> 드럽게 많다.
-
-해결 : w혹은 h로 큰 수를 모두 몰아준다음 구하면, 최솟값이 된다.
-
-
-*/
