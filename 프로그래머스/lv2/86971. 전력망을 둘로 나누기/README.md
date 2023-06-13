911c3c853930b1b5dacd4e4f92581c64393678dd
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-06-13 14:37:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.94 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,62 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] wires) {
-        if (n == 2) return 0;
-        
-        int answer = Integer.MAX_VALUE;    
-        
-        boolean[][] isLinked = new boolean[n + 1][n + 1];
-        
-        // 그래프 만들기
-        for (int[] wire : wires) {
-            isLinked[wire[0]][wire[1]] = true;
-            isLinked[wire[1]][wire[0]] = true;
-        }
-        
-        // 하나씩 지웠다 추가하기
-        for (int i = 0; i < n - 1; i++) {
-            
-            int a = wires[i][0];
-            int b = wires[i][1];
-            
-            if (isLinked[a][b]) {
-                // 끊기
-                isLinked[a][b] = false;
-                isLinked[b][a] = false;
-
-                // 순회
-                answer = Math.min(findDiff(isLinked, a, n), answer); 
-
-                // 복구
-                isLinked[a][b] = true;
-                isLinked[b][a] = true;
-             }
-        }
-        
+        int answer = -1;
         return answer;
-    }
-    
-    public int findDiff(boolean[][] isLinked, int start, int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-        int count = 1;
-        
-        visited[start] = true;
-        queue.offer(start);
-        
-        while (!queue.isEmpty()) {
-            start = queue.poll();
-            
-            for (int i = 1; i < n + 1; i++) {
-                if (isLinked[start][i] && !visited[i]) {
-                    visited[i] = true;
-                    queue.offer(i);
-                    count++;
-                }
-            }
-        }
-        
-        return Math.abs(n - count - count); // (n - count) - count
     }
 }
