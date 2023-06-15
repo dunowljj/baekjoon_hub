922f4ceb344c7427aec51d3e6b436e0b10ab2e27
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-06-16 02:05:17 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 268 ms, Memory: 20032 KB -BaekjoonHub

--- 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
@@ -1,27 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        char[] alphabets = new char[26];
-        for (int i = 0; i < 26; i++) alphabets[i] = (char) (i + 65);
         int[] val = {3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1};
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < 26; i++) map.put(alphabets[i], val[i]);
 
         int answer = 0;
         for (int i = 0; i < input.length(); i++) {
-            answer += map.get(input.charAt(i));
+            answer += val[input.charAt(i) - 65];
         }
 
         String result = (answer % 10) % 2 == 1 ?  "I'm a winner!" : "You're the winner?";
+        
         System.out.println(result);
     }
 }
