e7e74f9f488801fc48039f937bbf25709b414af7
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-06-23 21:05:38 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 9.10 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static final double epsilon = 0.0001d;
+    
+    public long solution(long r1, long r2) {    
+        long answer = 0;
+        double count2;
+        double count1;
+        
+        for (long i = 1; i < r2; i++) {
+            if (i < r1) {
+                count2 = Math.sqrt(r2 * r2 - i * i);
+                count1 = Math.sqrt(r1 * r1 - i * i);
+                
+                // x축에 붙어있지 않는 경우 && 작은 원의 한 지점의 높이가 정수인 경우
+                int count1Int = (int) count1;
+                if (count1 - count1Int == 0.0) answer++;
+            
+                answer += (long) count2 - (long) count1;   
+            }
+            
+            else {
+                count2 = Math.sqrt(r2 * r2 - i * i);
+                
+                answer += (long) count2;
+            }
+        }
+               
+        // 축의 진행 4방향 중 한방향에 존재하는 점의 개수 더하기
+        answer += r2 - r1 + 1;
+        
+        return 4 * answer;
+    }
+    
+   
+}
