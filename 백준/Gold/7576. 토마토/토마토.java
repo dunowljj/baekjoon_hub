b0e0c8d13bf9ca6ed564737dac24737fcaf38e16
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-07-13 12:16:07 +0900

    [Gold V] Title: 토마토, Time: 644 ms, Memory: 121260 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public class Main {
 
-    public static final int RIPEN = 1;
-    public static final int EMPTY = -RIPEN;
-    public static final int NOT_RIPEN = 0;
+    public static final int RIPE = 1;
+    public static final int EMPTY = -1;
+    public static final int UNRIPE = 0;
     static int[][] mapper = {{0, 0, -1, 1}, {1, -1, 0, 0}};
-
-    static int answer = 0;
+    static int day = 0;
+    static int unripeCount = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,38 +20,44 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int[][] box = new int[N][M];
 
-        boolean alreadyRipen = true; // 주어진 박스에 토마토가 모두 익은 경우 체크
-        Queue<Point> queue = new LinkedList<>();
+        Queue<Point> queue = new LinkedList<>(); // 익은 토마토 미리 넣는 큐
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 int tomatoStatus = Integer.parseInt(st.nextToken());
                 box[i][j] = tomatoStatus;
 
-                if (tomatoStatus == NOT_RIPEN) alreadyRipen = false;
-                else if (tomatoStatus == RIPEN) queue.offer(new Point(i, j));
-            }
-        }
-
-        if (alreadyRipen) {
-
-        } else {
-            while (true) {
-                bfs(queue, box, N, M); // 하루치만 탐색하고 큐에 데이터 유지
-                answer++;
-                if (areAllRipen(box, N, M)) break;
-                // 모두 익지 않았는데 큐가 비어있으면 모두 익지 못하는 상황이다.
-                else if (queue.isEmpty()) {
-                    answer = -1;
-                    break;
+                if (tomatoStatus == UNRIPE) {
+                    unripeCount++;
+                } else if (tomatoStatus == RIPE) {
+                    queue.offer(new Point(i, j));
                 }
             }
         }
 
-        System.out.print(answer);
+        while (true) {
+            // 이미 다 익었으면 0인 day값 출력
+            if (areAllUnripe(unripeCount)) break;
+
+            // 모두 익지 않았는데 큐가 비어있으면 모두 익지 못하는 상황이다.
+            else if (queue.isEmpty()) {
+                day = -1;
+                break;
+            }
+
+            passOneDay(queue, box, N, M); // 하루치만 탐색하고 큐에 데이터 유지
+            day++;
+        }
+
+        System.out.print(day);
     }
 
-    private static void bfs(Queue<Point> queue, int[][] box, int n, int m) {
+    private static boolean areAllUnripe(int unripeCount) {
+        return unripeCount == 0;
+    }
+
+    private static void passOneDay(Queue<Point> queue, int[][] box, int n, int m) {
         int size = queue.size();
 
         for (int i = 0; i < size; i++) {
@@ -61,24 +67,16 @@ public class Main {
                 int nx = now.x + mapper[0][dir];
                 int ny = now.y + mapper[1][dir];
 
-                // 이미 익었거나, 빈칸은 탐색하지 않는다. -> 익지 않은 경우를 제외하고 탐색하지 않음
+                // 이미 익었거나, 빈칸은 탐색하지 않는다. -> "익지 않은 경우(UNRIPE)"를 제외하고 탐색하지 않음
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m
-                        || box[nx][ny] != NOT_RIPEN) continue;
+                        || box[nx][ny] != UNRIPE) continue;
 
-                box[nx][ny] = RIPEN;
+                box[nx][ny] = RIPE;
+                unripeCount--;
                 queue.offer(new Point(nx, ny));
             }
         }
 
-    }
-
-    private static boolean areAllRipen(int[][] box, int n, int m) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (box[i][j] == NOT_RIPEN) return false;
-            }
-        }
-        return true;
     }
 
     static class Point {
@@ -98,8 +96,9 @@ public class Main {
 
 문제 : 며칠이 지나야 모두 익는가?
 
-익었는지 검사할때마다 N*M(100만) 만큼 순회
-100만을 순회하면서 4개씩 탐색?
-익은 토마토 위치를 기억하는게 의미가 있을까?
+[시간]
+(익은 토마토의 위치의 수 * 4)를 최대 N+M번 탐색 -> 익은 토마토의 위치에 따라 연산 수가 상이하다.
 
+[공간]
+N*M
  */
