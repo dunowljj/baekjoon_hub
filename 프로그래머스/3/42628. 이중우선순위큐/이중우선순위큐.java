72a7b2fd8f170e6f6da0cf80c94a1e840522a3ce
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-12-28 16:53:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.97 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,7 +4,8 @@ class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
 
-        TreeSet<Integer> treeSet = new TreeSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        TreeMap<Integer, Integer> treeMap = new TreeMap();
 
         for (String operation : operations) {
             int num = Integer.parseInt(
@@ -12,25 +13,22 @@ class Solution {
             );
 
             if (operation.startsWith("I")) {
-                treeSet.add(num);
-                continue;
+                treeMap.put(num, treeMap.getOrDefault(num , 0) + 1);
             }
 
             // 최솟값 삭제
-            if (!treeSet.isEmpty() && operation.startsWith("D -1")) {
-                treeSet.remove(treeSet.first());
-                continue;
+            else if (!treeMap.isEmpty() && operation.startsWith("D -")) {
+                treeMap.remove(treeMap.firstKey());
             }
 
             // 최댓값 삭제
-            if (!treeSet.isEmpty() && operation.startsWith("D 1")) {
-                treeSet.remove(treeSet.last());
-                continue;
+            else if (!treeMap.isEmpty() && operation.startsWith("D")) {
+                treeMap.remove(treeMap.lastKey());
             }
         }
 
-        if (!treeSet.isEmpty()) {
-            answer = new int[]{treeSet.last(), treeSet.first()};
+        if (!treeMap.isEmpty()) {
+            answer = new int[]{treeMap.lastKey(), treeMap.firstKey()};
         }
 
         return answer;
@@ -39,4 +37,5 @@ class Solution {
 /**
  * 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제
  * 큐가 비어있다면 삭제연산 무시
+ * 중복된 값의 처리를 신경쓰지 못했다. --> Map으로 바꿔야할듯
  */
