215d6cfc905fafb8216b80e6e0ca8cbe4f57d37a
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-12-28 23:58:36 +0900

    [level 2] Title: 가장 큰 수, Time: 4.49 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,23 +2,26 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
-        // 6, 62, 67, 600, 0
-        // 06 60
+        
         StringBuilder sb = new StringBuilder();
-
-        // 67 6 --> 676 667
         Arrays.stream(numbers)
                 .mapToObj(String::valueOf)
                 .sorted((o1, o2) -> (o2+o1).compareTo(o1+o2))
                 .map(String::valueOf)
                 .forEach((s) -> sb.append(s));
 
-        int idx = 0;
-        while (idx < sb.length() && sb.charAt(idx) == '0') {
-            idx++;
-        }
-
-        if (idx == sb.length()) return "0";
-        else return sb.substring(idx);
+       if (sb.charAt(0) == '0') return "0";
+       else return sb.toString();
     }
 }
+
+// 6, 62, 67, 600, 60
+// 60 06
+// 67 6 --> 676 667
+
+/**
+ * 두 문자열을 합친 것을 사전순으로 비교해서 내림차순으로 정렬
+ * 
+ * 내림차순 정렬이므로 0으로 시작하는 값이 있다면 뒤에 오는 값들도 모두 0이다.
+ * 0은 연결시켜도 1개의 0을 반환한다.
+ */
