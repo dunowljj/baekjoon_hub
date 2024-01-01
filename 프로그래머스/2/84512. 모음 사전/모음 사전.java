542acd497d2586b45145339f66ffd957e2b53d9d
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-01 18:55:16 +0900

    [level 2] Title: 모음 사전, Time: 0.57 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -3,25 +3,32 @@ import java.util.Map;
 
 class Solution {
 
+    private static boolean finished = false;
     private static int order = 1;
     private static final char[] chars = {'A', 'E', 'I', 'O', 'U'};
 
     public int solution(String word) {
-        Map<String, Integer> dict = new HashMap<>();
-        makeDictionary(dict, new StringBuilder(),0);
-        return dict.get(word);
+        for (int i = 0; i < chars.length; i++) {
+            makeDictionary(new StringBuilder().append(chars[i]), 1, word);
+        }
+        return order;
     }
 
-    private void makeDictionary(Map<String, Integer> dict, StringBuilder sb, int depth) {
-        if (!sb.toString().isEmpty() && !dict.containsKey(sb.toString())) {
-            dict.put(sb.toString(), order++);
+    private void makeDictionary(StringBuilder sb, int depth, String word) {
+        if (finished) return;
+        
+        if (sb.toString().equals(word)) {
+            finished = true;
+            return;
         }
+        
+        order++;
 
         if (depth == 5) return;
 
         for (int i = 0; i < 5; i++) {
             sb.append(chars[i]);
-            makeDictionary(dict, sb, depth + 1);
+            makeDictionary(sb, depth + 1, word);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
