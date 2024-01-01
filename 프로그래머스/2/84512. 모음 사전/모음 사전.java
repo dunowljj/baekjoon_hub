11106cfdac6c7f413b9e5619796975a0047badea
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-01 19:05:41 +0900

    [level 2] Title: 모음 사전, Time: 0.86 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -4,31 +4,30 @@ import java.util.Map;
 class Solution {
 
     private static boolean finished = false;
-    private static int order = 1;
+    private static int order = 0;
+    private static int answer = 0;
     private static final char[] chars = {'A', 'E', 'I', 'O', 'U'};
 
     public int solution(String word) {
-        for (int i = 0; i < chars.length; i++) {
-            makeDictionary(new StringBuilder().append(chars[i]), 1, word);
-        }
-        return order;
+        findOrder(new StringBuilder(), 0, word);
+        return answer;
     }
 
-    private void makeDictionary(StringBuilder sb, int depth, String word) {
+    private void findOrder(StringBuilder sb, int depth, String word) {
         if (finished) return;
-        
+
         if (sb.toString().equals(word)) {
+            answer = order;
             finished = true;
             return;
         }
-        
-        order++;
 
         if (depth == 5) return;
 
         for (int i = 0; i < 5; i++) {
             sb.append(chars[i]);
-            makeDictionary(sb, depth + 1, word);
+            order++;
+            findOrder(sb, depth + 1, word);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
