bb55adfe7eeaab6d1a3778f24569fe6ff249ce5c
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-10 20:27:21 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,37 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int M;
     static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[M];
-        dfs(0,0);
-        System.out.println(sb);
-    }
-    public static void dfs(int depth, int start){
 
-        if(depth == M){
-            for(int num : arr){
-                sb.append(num).append(' ');
+
+        dfs(arr, 0, 0);
+        bw.write(sb.toString().trim());
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int[] arr, int depth, int start) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
 
-        for(int i=start; i<N; i++){
-            arr[depth] = i+1;
-            dfs(depth+1, i+1);
+        for (int i = start; i < N; i++) {
+            arr[depth] = i + 1; 
+            dfs(arr, depth + 1, i + 1);
         }
     }
 }
