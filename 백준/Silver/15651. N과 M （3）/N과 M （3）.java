0bb8b2689530767fe171794ac496a43473c54e20
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-15 23:14:21 +0900

    [Silver III] Title: N과 M (3), Time: 400 ms, Memory: 77628 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -4,30 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int arr[];
+
+    public static final String LINE_BREAK = System.lineSeparator();
+    public static final String SPACE = " ";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        int depth = 0;
-        dfs(N, M, depth);
-        System.out.println(sb);
+
+        // 1~N 초기화
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = i + 1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        dfs(0, N, M, new int[M], sb);
+
+        System.out.print(sb.toString().trim());
     }
-    public static void dfs(int N, int M, int depth){
-        if (M == depth) {
-            for(int num : arr) {
-                sb.append(num).append(" ");
-            }
-            sb.append("\n");
+
+    private static void dfs(int depth, int n, int m, int[] seq, StringBuilder sb) {
+        if (depth == m) {
+            append(seq, sb);
             return;
         }
 
-        for(int i=0; i<N; i++){
-            arr[depth] = i+1;
-            dfs(N, M , depth+1);
+        for (int i = 1; i <= n; i++) {
+                seq[depth] = i;
+                dfs(depth + 1, n, m, seq, sb);
         }
+    }
+
+    private static void append(int[] seq, StringBuilder sb) {
+        for (int num : seq) {
+            sb.append(num).append(SPACE);
+        }
+        sb.append(LINE_BREAK);
     }
 }
