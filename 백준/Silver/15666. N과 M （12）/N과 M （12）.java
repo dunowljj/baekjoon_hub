077ccd0b3135577fb73c3f319ef82b5f97efbd63
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-20 22:45:58 +0900

    [Silver II] Title: N과 M (12), Time: 136 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -7,9 +7,8 @@ public class Main {
     static int M;
     static int N;
     static StringBuilder sb = new StringBuilder();
-    static Set<Integer> set = new LinkedHashSet<>();
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -21,14 +20,10 @@ public class Main {
         arr = new int[M];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            inputNums[i] = Integer.parseInt(st.nextToken());
         }
 
-        Iterator<Integer> iter = set.iterator();
-        for (int i = 0; i < set.size(); i++) {
-            inputNums[i] = iter.next();
-        }
         Arrays.sort(inputNums);
 
         dfs(0, 0);
@@ -38,7 +33,7 @@ public class Main {
         bw.close();
     }
 
-    static void dfs(int depth, int start) {
+    static void dfs(int depth, int idx) {
         if (depth == M) {
             for (int num : arr) {
                 sb.append(num).append(" ");
@@ -47,20 +42,17 @@ public class Main {
             return;
         }
 
-        int before = 0;
-        for (int i = start; i < N; i++) {
+        else{
+            int before = 0;
+            for (int i = idx; i < N; i++) {
 
-            if (before != inputNums[i]) {
-                arr[depth] = inputNums[i];
-                before = inputNums[i];
-                dfs(depth + 1, i);
+                if (before != inputNums[i]) {
+                    arr[depth] = inputNums[i];
+                    before = inputNums[i];
+                    dfs(depth + 1, i);
+                }
             }
         }
     }
 
 }
-/*
-같은 수가 여러개 주어질 수 있음 + 같은 수 여러번 고를 수 있음
-방법1 : 방문여부 제거하고 set 으로 입력받은 숫자를 거르기, 시작점 부여
-
- */
