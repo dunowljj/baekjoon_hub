e4f65340c6845bff1f9ae6688d6a40556148d4a1
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-22 23:22:59 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 904 ms, Memory: 201452 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static final int IDX_CONVERT = 10_000_000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,31 +13,22 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] cardsCount = new int[20_000_001];
-        // (-10_000_000 ~ -1 ) + 10_000_000 -> 1~ (10_000_000 -1)
-        // 0~10_000_000  + 10_000_000 -> 10_000_000 ~ 20_000_000
 
         // 가지고 있는 카드들의 수에 해당하는 인덱스에 ++해서 카운트 0을 고려 안했음;;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int input = Integer.parseInt(st.nextToken());
-            int idx = input + 10_000_000;
-
-            cardsCount[idx]++;
+            cardsCount[input + IDX_CONVERT]++;
         }
 
         int M = Integer.parseInt(br.readLine());
-        int[] answer = new int[M];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int curr = Integer.parseInt(st.nextToken());
-            int idx = curr + 10_000_000;
+            int count = cardsCount[curr + IDX_CONVERT];
 
-            answer[i] = cardsCount[idx];
-        }
-
-        for (int num : answer) {
-            sb.append(num).append(" ");
+            sb.append(count).append(" ");
         }
 
         bw.write(sb.toString().trim());
