cb29cb085f9147fa53e6a10ae7360fc8bc848870
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-23 20:20:37 +0900

    [Silver IV] Title: 수 찾기, Time: 640 ms, Memory: 42420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,37 +1,52 @@
-import java.io.*;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder answer = new StringBuilder();
-        Set<Integer> set = new TreeSet<>();
-
         int N = Integer.parseInt(br.readLine());
 
+        int[] nums = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            set.add(num);
+            nums[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(nums);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= M; i++) {
-            int num = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            int searchingNum = Integer.parseInt(st.nextToken());
 
-            if ( set.contains(num)) {
-                answer.append(1).append("\n");
+            boolean isExist = binarySearch(N, nums, searchingNum);
+            answer.append(isExist ? 1 : 0).append(System.lineSeparator());
+        }
+
+        System.out.println(answer.toString().trim());
+    }
+
+    private static boolean binarySearch(int n, int[] nums, int searchingNum) {
+        int lo = 0;
+        int hi = n;
+
+        // F F F F T T(n)
+        // F F T T T  T(n)
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (nums[mid] >= searchingNum) {
+                hi = mid;
             } else {
-                answer.append(0).append("\n");
+                lo = mid + 1;
             }
         }
 
-        bw.write(answer.toString().trim());
-        bw.flush();
-        bw.close();
+        return lo != n && searchingNum == nums[lo];
     }
 }
