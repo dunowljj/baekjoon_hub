96995b688bc9239afe104db50970e5bcb3b49ecb
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-25 22:11:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 16952 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,46 +1,46 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken()); // 1 ~ 10_000
-        int N = Integer.parseInt(st.nextToken()); // 1 ~ 1_000_000
 
-        List<Integer> lines = new ArrayList<>();
-        long end = 0;
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] lens = new int[K];
+        long max = 0;
         for (int i = 0; i < K; i++) {
-            int line = Integer.parseInt(br.readLine());
-            lines.add(line);
-            if (line > end) end = line;
+            int len = Integer.parseInt(br.readLine());
+            max = Math.max(max, len);
+            lens[i] = len;
         }
 
-        long start = 0L;
-        long mid = 0L;
-        end++;
+        // T T T T F F F
+        long lo = 0;
+        long hi = max + 1;
+        while (lo + 1 < hi) {
+            long mid = (lo + hi) / 2;
 
-        while (start < end) {
-            mid = (start + end) / 2;
-            
-            if (isCuttable(mid, lines, N)) {
-                start = mid + 1;
+            if (canMake(mid, lens, N)) {
+                lo = mid;
             } else {
-                end = mid;
+                hi = mid;
             }
         }
 
-        System.out.println(end - 1L);
+        System.out.print(lo);
     }
 
-    private static boolean isCuttable(long size, List<Integer> lines, int need) {
-        int pieces = 0;
-        for (Integer line : lines) {
-            pieces += line / size;
+    private static boolean canMake(long cutSize, int[] lens, int needCount) {
+        long count = 0;
+        for (int len : lens) {
+            count += len / cutSize;
         }
 
-        return pieces >= need;
+        return count >= needCount;
     }
 }
