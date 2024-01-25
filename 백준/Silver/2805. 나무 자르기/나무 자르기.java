35d0b59f92feae762211f5f623a1d7846cfc33ef
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-01-25 21:45:48 +0900

    [Silver II] Title: 나무 자르기, Time: 580 ms, Memory: 123336 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,58 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static final int MIN_CUT_HEIGHT = 0;
-    private static final int MAX_CUT_HEIGHT = 1_000_000_000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
 
+        long[] heights = new long[N];
         st = new StringTokenizer(br.readLine());
-        List<Integer> heights = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            heights.add(Integer.parseInt(st.nextToken()));
+            long height = Long.parseLong(st.nextToken());
+            heights[i] = height;
         }
 
-        int low = MIN_CUT_HEIGHT;
-        int high = MAX_CUT_HEIGHT;
-        while (low + 1 < high) {
+        // T T T T F F F
+        long lo = 0L;
+        long hi = 2_000_000_001L;
 
-            int mid = (low + high) / 2;
+        while (lo + 1 < hi) {
+            long mid = (lo + hi) / 2L;
 
-            // T T T T T F F F F
-            if (isEnough(mid, M, heights)) {
-                low = mid;
+            if (satisfied(mid, heights, M)) {
+                lo = mid;
             } else {
-                high = mid;
+                hi = mid;
             }
         }
 
-        System.out.print(low);
+        System.out.print(lo);
     }
 
-    private static boolean isEnough(int cutHeight, int M, List<Integer> heights) {
-        long sum = 0;
-        for (int height : heights) {
-            sum += remainHeightAfterCut(cutHeight, height);
+    private static boolean satisfied(long cutHeight, long[] heights, long needHeight) {
+        long collected = 0;
+        for (long height : heights) {
+            collected += Math.max(0, height - cutHeight);
         }
 
-        return sum >= M;
+        return needHeight <= collected;
     }
 
-    private static int remainHeightAfterCut(int cutHeight, int height) {
-        int remain = height - cutHeight;
-
-        if (remain < 0 ) return 0;
-        else return remain;
-    }
+    // 누적합쓰면?
 }
