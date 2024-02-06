6ce4db327cee0861e696b842943e4fe9c3d64432
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-02-06 14:32:03 +0900

    [Gold IV] Title: 네트워크 연결, Time: 520 ms, Memory: 46732 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,74 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.*;
-
-import static java.util.Comparator.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Connect {
-        int no;
-        int cost;
-
-        public Connect(int no, int cost) {
-            this.no = no;
-            this.cost = cost;
-        }
-
-        public Connect(int cost) {
-            this.cost = cost;
-        }
-
-        public int getCost() {
-            return cost;
-        }
-    }
-
-    static List<Connect>[] graph;
-    static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
-        graph = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
+        int[] parent = new int[N + 1];
+        for (int i = 1; i < parent.length; i++) parent[i] = i;
 
-        for (int i = 0; i < N + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            graph[a].add(new Connect(b, c));
-            graph[b].add(new Connect(a, c));
+            pq.offer(new Edge(a, b, cost));
         }
 
-        int sum = 0;
-        PriorityQueue<Connect> pq = new PriorityQueue<>(comparingInt(Connect::getCost));
-        pq.offer(new Connect(1, 0));
+        int cost = 0;
         while (!pq.isEmpty()) {
-            Connect now = pq.poll();
+            Edge edge = pq.poll();
 
-            if (visited[now.no]) continue;
-            visited[now.no] = true;
-            sum += now.cost;
-
-            for (Connect next :graph[now.no]){
-                pq.offer(next);
+            if (find(parent, edge.a) != find(parent, edge.b)) {
+                union(parent, edge.a, edge.b);
+                cost += edge.cost;
             }
+
         }
 
-        System.out.println(sum);
+        System.out.print(cost);
+    }
+
+    private static int find(int[] parent, int no) {
+        if (parent[no] == no) return no;
+        else return parent[no] = find(parent, parent[no]);
+    }
+
+    private static void union(int[] parent, int a, int b) {
+        int p1 = find(parent, a);
+        int p2 = find(parent, b);
+
+        if (p1 != p2) {
+            parent[p1] = p2;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int a;
+        int b;
+        int cost;
+
+        public Edge(int a, int b, int cost) {
+            this.a = a;
+            this.b = b;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge e) {
+            return this.cost - e.cost;
+        }
     }
 }
+/**
+ * 모든 컴퓨터 직간접적 연결 필요
+ * 최소 비용 -> MST
+ *
+ * 최대 비용 합 10억 (int)
+ */
