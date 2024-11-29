381296096f4ec3c455c1eed295f4ba3ac57875c0
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-11-29 17:15:52 +0900

    Update 도둑질.java

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -19,14 +19,14 @@ class Solution {
         // dp_exclude[n] 맨 앞집을 털지 않은 경우 n번째까지 최댓값
         dp_exclude[0] = 0;
         dp_exclude[1] = money[1];
-        // dp_exclude[2] = Math.max(money[1], money[2]);
         
         // dp_include[n] 맨 앞집을 털은 것을 포함한 경우 -> 맨 뒷집을 털 수 없다.
         dp_include[0] = money[0];
-        dp_include[1] = Math.max(money[0], money[1]); // 털지 않고 맨끝 집을 사용한 경우가 더 많을 수도 있다.
-        // dp_include[2] = money[0] + money[2]; 
-        // 1 3 5 6 9 4
+        dp_include[1] = Math.max(money[0], money[1]); 
+        
+        // 털지 않고 맨끝 집을 사용한 경우가 더 많을 수도 있다.
         // 0 3 1 1 9 1
+        // 0 0 3 1 1 9
         
         for (int i = 2; i < n; i++) {
             dp_exclude[i] = Math.max(dp_exclude[i - 2] + money[i], dp_exclude[i - 1]);
@@ -38,8 +38,8 @@ class Solution {
 }
 /**
 인접한 집을 2집을 털면 안된다.
+양끝도 안되는듯
 
-양끝도 안되는듯?
 2집
 ox
 xo
@@ -59,12 +59,4 @@ oxxox
 xoxox
 xoxxo
 xxoxo
-
-
-
-
-x o x o o
-
-맨 앞집을 턴 경우와 그렇지 않은경우를 나눠봐야하나?
-
 **/
