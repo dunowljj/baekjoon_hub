fd911c23199c2ea98fd1935cc01e56f91d47cb1f
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-01-25 16:39:37 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 100 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -15,20 +15,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
+        initSumArr();
 
+        int T = Integer.parseInt(br.readLine());
         List<Integer> distances = new ArrayList<>();
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            distances.add(end - start);
+            findMoveCount(end - start);
         }
-
-        // 누적합 배열 생성
-        initSumArr();
-        distances.forEach(Main::findMoveCount);
 
         System.out.print(answer.toString().trim());
     }
@@ -44,29 +41,43 @@ public class Main {
     private static void findMoveCount(int dist) {
         int half = dist / 2;
 
+        int i = findFirstOverHalf(half);
+
         int move = 0;
-        for (int i = 1; i < sum.length; i++) {
-            if (sum[i] > half) {
+        if (sum[i] + sum[i - 1] <= dist) {
+            move = i + (i - 1);
 
-                if (sum[i] + sum[i - 1] <= dist) {
-                    move = i + (i - 1);
+            // 남는 거리가 있다면 한번더 움직여야함
+            if (sum[i] + sum[i - 1] < dist) move ++;
+        }
 
-                    // 남는 거리가 있다면 한번더 움직여야함
-                    if (sum[i] + sum[i - 1] < dist) move ++;
-                }
+        // sum[i]가 half의 첫 초과 시점이므로 (sum[i-2] * 2) <= dist
+        else {
+            move = (i - 1) * 2;
 
-                // sum[i]가 half의 첫 초과 시점이므로 (sum[i-2] * 2) <= dist
-                else {
-                    move = (i - 1) * 2;
+            // 남는 거리가 있다면 한번더 움직여야함
+            if (sum[i - 1] * 2 < dist) move++;
+        }
 
-                    // 남는 거리가 있다면 한번더 움직여야함
-                    if (sum[i - 1] * 2 < dist) move++;
-                }
+        answer.append(move).append(System.lineSeparator());
+    }
 
-                break;
+    private static int findFirstOverHalf(int half) {
+        int lo = 0;
+        int hi = sum.length - 1;
+
+        // F F T T T
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (sum[mid] > half) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
             }
         }
-        answer.append(move).append(System.lineSeparator());
+
+        return lo;
     }
 }
 /**
@@ -74,23 +85,14 @@ public class Main {
  * 그렇다면 직전-1 번째 이동은 2광년 혹은 1광년이 된다.
  *
  * x, y는 int의 최댓값까지 범위로 21억정도 될듯.
- * 0이 9개 -> mb gb
+ * 배열만들면 0이 9개 -> mb 2gb
  *
  *
  * 가정. 가장 큰수까지 찍고 오는게 가장 빠르게 가는 방법일까?
  *
  * n(n-1) / 2 -> 68000 제곱정도 되면 int의 max에 도달한다.
  *
- *
- * 1) 1에서 시작, 증가시키면서 더하기
- * 2) 상황별 대처
- * - 더한 합이 구하려는 수의 절반과 같다면, 그때부터 감소시키면 딱 맞음 -> 홀수인 경우 애메함
- * - 더한 합이 구하려는 수의 절반보다 크다면, 더할지 더하지 않을지 판별 필요
- * - 더한 합이 구하려는 수의 절반보다 작다면, 더할지 더하지 않을지 판별 필요. 위 항목과 겹침
- *
- *
- *
- *  * ### 고민
+ *  ### 고민
  *  여러개 케이스가 많은데 이거 감당이 되는가? 케이스가 몇갠지 설명이 없다.
  *  -->> 누적합 배열을 만들자!!
  *
@@ -109,8 +111,9 @@ public class Main {
  *
  *
  * 결국 특정 누적합이 half 보다 크기 시작했을때 기점으로 검사
- * 1) 값이 half보다 클때 sum[n] + sum[n-1]이 거리를 초과한다면
+ * 1) 값이 half보다 클때 sum[n] + sum[n-1]이 
  * 2) sum[n-1] * 2로 이동 구하기 (sum[n-1]은 half보다 같거나 작은 상태)
- *
- * 시간 부족하다면 이분탐색 쓰기
+ * --> 둘다 거리 초과하면 move + 1 해주기
+ * 
+ * 합 배열에서 half보다 처음으로 큰 위치 찾을때 이분탐색으로 시간 줄이기 쓰기
  */
