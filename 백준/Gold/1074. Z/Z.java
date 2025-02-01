50e9389dcb9b359ddf3c9649869af8c3bb46786f
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-02-01 20:06:59 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -6,61 +6,44 @@ import java.util.StringTokenizer;
 public class Main {
 
     private static int N;
-    private static int r;
-    private static int c;
-    private static int start;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
         int len = ((int) Math.pow(2, N));
         int count = len * len;
-        start = 0;
 
-        while (true) {
-
-            if (len == 1) {
-                break;
-            }
-
-            len /= 2; count /= 4;
-
-            // 왼위
-            if (r < len && c < len) {
-                // 그대로
-                continue;
-            }
-
-            // 오른위
-            if (r < len && c >= len) {
-                start += count;
-                c -= len;
-                continue;
-            }
-
-            // 왼아래
-            if (r >= len && c < len) {
-                start += count * 2;
-                r -= len;
-                continue;
-            }
-
-            // 오른아래
-            if (r >= len && c >= len) {
-                start += count * 3;
-                r -= len;
-                c -= len;
-                continue;
-            }
-        }
+        int start = find(r, c, 0, len, count);
         System.out.print(start);
     }
+
+    private static int find(int r, int c, int start, int len, int count) {
+        if (len == 1) return start;
+
+        len /= 2;
+        count /= 4;
+
+        // 왼위
+        if (r < len && c < len)
+            return find(r, c, start, len, count);
+
+        // 오른위
+        if (r < len && c >= len)
+            return find(r, c - len, start + count, len, count);
+
+        // 왼아래
+        if (r >= len && c < len)
+            return find(r - len, c, start + (count * 2), len, count);
+
+        // 오른아래
+        if (r >= len && c >= len)
+            return find(r - len, c - len, start + (count * 3), len, count);
+
+        return start;
+    }
 }
-/**
- *
- */
