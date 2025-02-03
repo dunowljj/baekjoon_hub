2a4b164747cc0e42818823a966176de30ae037fc
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-02-03 19:11:16 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 18576 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        System.out.print(dp[n1][n2]);
+    }
+}
