d411a8da076bf1a6e50b30a72ceebfe964e1863b
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-02-13 16:06:22 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.62 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 1.86 ms
+메모리: 72.6 MB, 시간: 0.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 13일 15:52:54
+2025년 02월 13일 16:06:21
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -42,26 +42,30 @@ class Solution {
         Queue<Point> queue = new LinkedList<>();
         queue.offer(new Point(y, x));
         visited[y][x] = true;
+        int depth = 0;
         
         while (!queue.isEmpty()) {
-            Point now = queue.poll();
+            int size = queue.size();
             
-            if (now.dist == 2) return true;
-            
-            for (int i = 0; i < 4; i++) {
-                int ny = now.y + mapper[0][i];
-                int nx = now.x + mapper[1][i];
-                int nd = now.dist + 1;
+            if (depth == 2) return true;
+            for (int s = 0; s < size; s++) {
+                Point now = queue.poll();
                 
-                if (ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) continue;
-                visited[ny][nx] = true;
-                
-                char next = place[ny].charAt(nx);
-                if (next == PARTITION) continue;
-                if (next == PERSON && nd <= 2) return false;
-                
-                queue.offer(new Point(ny, nx, nd));
+                for (int i = 0; i < 4; i++) {
+                    int ny = now.y + mapper[0][i];
+                    int nx = now.x + mapper[1][i];
+
+                    if (ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) continue;
+                    visited[ny][nx] = true;
+
+                    char next = place[ny].charAt(nx);
+                    if (next == PARTITION) continue;
+                    if (next == PERSON) return false;
+
+                    queue.offer(new Point(ny, nx));
+                }
             }
+            depth++;
         }
         
         return true;
@@ -70,18 +74,10 @@ class Solution {
     static class Point {
         int y;
         int x;
-        int dist;
         
         public Point(int y, int x) {
             this.y = y;
             this.x = x;
-            this.dist = 0;
-        }
-        
-        public Point(int y, int x, int dist) {
-            this.y = y;
-            this.x = x;
-            this.dist = dist;
         }
     }
 }
@@ -94,6 +90,9 @@ class Solution {
 
 맨해튼 거리가 아닌 실제 이동거리 기준으로 계산. 어짜피 파티션인 경우 막힌걸로 판단하기 때문이다.
 
- 
+### 주의 
+- 2이하 거리검증 부분을 사용하더라도 정확히 2depth를 탐색하지 않으면 문제가 생긴다.
+- 현재 시작점 기준으로 검증하는데, 방문체크를 해버리기때문에 다른 탐색을 방해할 수 있다.
+- ex) P O O P P O P -> 첫P에서 시작하면, 거리두기를 하지 않은 다른 P를 방문체크하여 다시 방문하지 않게 만듬.
 
 */
