1b7135ce4e7c367bcb4754f495194ff44347a8c5
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-02-28 15:14:54 +0900

    [level 4] Title: 호텔 방 배정, Time: 457.18 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,38 +1,65 @@
 import java.util.*;
 
 class Solution {
-    private static final Map<Long, Long> map = new HashMap<>();
+    
+    List<Long> answer;
+    Map<Long,Long> reserveMap = new HashMap<>();
     
     public long[] solution(long k, long[] room_number) {
-        int n = room_number.length;
-        long[] answer = new long[room_number.length];
+        answer = new ArrayList<>();
         
-        for (int i = 0; i < n; i++) {
-            long requestNumber = room_number[i];
-            answer[i] = find(requestNumber);
-        }        
-        
-        return answer;
-    }
-    
-    private long find(long roomNumber) {
-        if (!map.containsKey(roomNumber)) {
-            map.put(roomNumber, roomNumber + 1);
-            return roomNumber;
+        for (long requestRoomNo : room_number) {
+           long find = findRoom(requestRoomNo);
+           answer.add(find);
+           reserveMap.put(find, find + 1);
         }
         
-        // 경로 압축
-        long next = find(map.get(roomNumber));
-        map.put(roomNumber, next);
-        return next;
+        // for (Map.Entry<Long,Long> e : reserveMap.entrySet()) {
+        //     System.out.println("k:"+e.getKey()+",v:"+e.getValue());
+        // }
+        
+        return answer.stream().mapToLong(Long::longValue).toArray();
     }
+    
+    // 요청에 대한 빈방을 찾는다. 빈방을 찾으면서 경로를 압축한다.
+    private Long findRoom(long requestNo) {
+        
+        // 빈방
+        if (!reserveMap.containsKey(requestNo)) {
+            return requestNo;
+        }
+        
+        // 빈방 아님
+        long find = findRoom(reserveMap.get(requestNo));
+        reserveMap.put(requestNo, find + 1);
+        return find;
+    }
+    
+//     private Long find(long requestNo) {
+//         if (!reserveMap.containsKey(requestNo)) {
+//             return requestNo;
+//         }
+
+//         return find(reserveMap.get(requestNo));
+//     }
+
+//     private void reserve(long requestNo) {
+//         reserveMap.put(reqeustNo, find(reuestNo + 1));
+//     }
 }
+
 /**
-k 10^12로 1조다. 4b kb mb gb
-TreeSet쓰면 초기화 비용이 너무 크다. 배열을 쓴다면 4gb?
+1~k번 방 존재
+처음에 모두 비어있음
+1명씩 신청, 방 번호 제출 -> 방이 있으면 배정 -> 이미 배정되어있으면 더 큰 방 중 가장 작은 방 배정
 
-Set에 넣고 체크하기엔, 시간 오래걸린다.
-10만개 요소가 연속하여 set에 있고, 그중 제일 낮은 방을 예약하려한다면 10만개 탐색해야한다.
-연결시켜서 마지막 요소의 참조를 공유하게 할수는 없나? 값을 넣으면서 다음 요소중 가장 큰 요소를 부모로 삼게한다.
+특정 방을 가르키면, 다음 빈 방을 가르키도록 유도해야함.
 
-**/
+### 시간제한 분석
+k 범위 1 ~ 10^12
+- 배열 선언 메모리 초과
+- HashSet에 값을 넣고, 1씩 증가시킨다? -> 요청이 1번방만 20만번 들어온다면,  (1+20만)*10만 -> 시간초과
+- HashMap<Long,Long> Key:예약된 방, Value:다음 예약할 방. 단, 경로를 압축시키면서 갱신하자.
+
+
+*/
