3bf6cbf6dc149cddb40d7aab6a7b64b7caeec8b1
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-03-25 18:23:23 +0900

    [level 3] Title: 캠핑, Time: 2712.12 ms, Memory: 199 MB -BaekjoonHub

--- 프로그래머스/3/1833. 캠핑/README.md
+++ 프로그래머스/3/1833. 캠핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] 캠핑 - 1833 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1833) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1833#) 
 
 ### 성능 요약
 
-메모리: 97.5 MB, 시간: 8221.09 ms
+메모리: 199 MB, 시간: 2712.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 16:45:32
+2025년 03월 25일 18:23:21
 
 ### 문제 설명
 
--- 프로그래머스/3/1833. 캠핑/캠핑.java
+++ 프로그래머스/3/1833. 캠핑/캠핑.java
@@ -1,45 +1,99 @@
 import java.util.*;
 
 class Solution {
+    
+    Map<Integer, Integer> xIdxMap = new HashMap<>();
+    Map<Integer, Integer> yIdxMap = new HashMap<>();
+    
+    TreeSet<Integer> yTree = new TreeSet<>();
+    TreeSet<Integer> xTree = new TreeSet<>();
+    
+    int[][] sum;
     public int solution(int n, int[][] data) {
         int answer = 0;
         
-        Arrays.sort(data, (o1, o2) -> {
-             if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        });
-            
         for (int i = 0; i < data.length; i++) {
-            for (int j = i + 1; j< data.length; j++) {
-                // 넓이 0
-                if (data[i][0] == data[j][0] || data[i][1] == data[j][1]) continue;
-                
-                if (canSetUp(data, i, j)) answer++;
+            xTree.add(data[i][0]);
+            yTree.add(data[i][1]);
+        }
+        
+        // treeSet을 활용하여 배열의 인덱스 계산. wedge가 있는 위치에 +1 해주기
+        sum = new int[yTree.size() + 1][xTree.size() + 1];
+        for (int i = 0; i < data.length; i++) {
+            int xIdx = xTree.headSet(data[i][0], true).size(); // [0]라인 공집합 활용을 위해 인덱스를 +1 시킨 상태
+            int yIdx = yTree.headSet(data[i][1], true).size();
+            
+            // System.out.print("["+data[i][1]+","+data[i][0]+"] ");
+            data[i][0] = xIdx;
+            data[i][1] = yIdx;
+            
+            sum[yIdx][xIdx] = 1;
+        }
+        // System.out.println();
+        
+        
+        // 2차원 누적합 계산
+        for (int i = 1; i < sum.length; i++) {
+            for (int j = 1; j < sum[i].length; j++) {
+                sum[i][j] += sum[i - 1][j];
             }
         }
-    
-        return answer;
+        for (int i = 1; i < sum.length; i++) {
+            for (int j = 1; j < sum[i].length; j++) {
+                sum[i][j] += sum[i][j - 1];
+            }
+        }
         
+//         for (int i = 0; i < sum.length; i++) {
+//             for (int j = 0; j < sum[i].length; j++) {
+//                 System.out.print(sum[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+        
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (canSetUp(data, i, j)) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
     }
     
     private boolean canSetUp(int[][] data, int i, int j) {
-        for (int k = i + 1; k < j; k++) {
-            if (
-                (data[i][0] < data[k][0] && data[k][0] < data[j][0]) &&
-                (Math.min(data[i][1],data[j][1]) < data[k][1] && data[k][1] < Math.max(data[i][1],data[j][1]))
-            ) return false;
-        }
+        int ix = data[i][0];
+        int iy = data[i][1];
         
-        return true;
+        int jx = data[j][0];
+        int jy = data[j][1];
+        
+        // 넓이 0
+        if (ix == jx || iy == jy) return false;
+        
+        // 압축한 좌표가 인접하면, 그 사이에 아무런 쐐기가 없다는 의미이다.
+        if (Math.abs(ix - jx) == 1 || Math.abs(iy - jy) == 1) return true;
+        
+        // 쐐기가 존재해서는 안되는 구역의 모서리 좌표
+        int leftX = Math.min(ix, jx) + 1;
+        int topY = Math.min(iy, jy) + 1;
+        
+        int rightX = Math.max(ix, jx) - 1;
+        int bottomY = Math.max(iy, jy) - 1;
+        
+        int wedgeCount = sum[bottomY][rightX] 
+            - sum[bottomY][leftX - 1] 
+            - sum[topY - 1][rightX] 
+            + sum[topY - 1][leftX - 1];
+        
+        return wedgeCount == 0;
     }
-    
 }
 /**
 설치할 수 있는 쐐기 수만 구하면 됨.
 안겹치게 최대한 많은 텐트를 설치하는게 아니다.
 결국 모든 경우에 대해 사이에 쐐기가 있는지만 체크하면된다.
 
-2차원 누적합List를 만들면 성능을 개선가능할듯
+2차원 누적합을 만들면 성능을 개선가능할듯
 **/
