d518dad5559bba259b9c16ba1a11efeabb359a91
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-05-06 08:51:13 +0900

    [Gold IV] Title: 두 배, Time: 328 ms, Memory: 35656 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long before = Integer.parseInt(st.nextToken());
+        int count = 0;
+        for (int i = 0; i < N - 1; i++) {
+            long e = Integer.parseInt(st.nextToken());
+
+            while (before > e) {
+                count++;
+                e *= 2;
+            }
+
+            before = e;
+        }
+
+        System.out.print(count);
+    }
+}
