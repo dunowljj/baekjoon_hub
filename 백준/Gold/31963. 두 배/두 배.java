c42b9fc64e12d0530cbea47c5a21001a2e8a07d6
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-05-06 15:15:53 +0900

    [Gold IV] Title: 두 배, Time: 352 ms, Memory: 36888 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -6,22 +6,33 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long before = Integer.parseInt(st.nextToken());
-        int count = 0;
-        for (int i = 0; i < N - 1; i++) {
-            long e = Integer.parseInt(st.nextToken());
+        long before = Long.parseLong(st.nextToken());
 
-            while (before > e) {
-                count++;
-                e *= 2;
+        long count = 0;
+        long[] counts = new long[N];
+        for (int i = 1; i < N; i++) {
+            long now = Long.parseLong(st.nextToken());
+
+            // 이전값이 더 커서 보정이 필요함
+            double beforePow = before * Math.pow(2, counts[i - 1]);
+            if (beforePow > now) {
+                counts[i] = counts[i - 1] + (long) Math.ceil(
+                        (Math.log((double) before / now) / Math.log(2))
+                );
+
+                count += counts[i];
             }
-
-            before = e;
+            before = now;
         }
 
         System.out.print(count);
     }
 }
+/**
+ * n[i-1] * 2^x <= n[i] * 2^y
+ * x + log2(n[i-1]) <= y + log2(n[i]);
+ * y >= x + log2(n[i-1]/n[i]);
+ */
