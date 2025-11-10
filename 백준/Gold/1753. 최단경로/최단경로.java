65cf6bdf820411fc3b0384ae123a8856dca78980
dunowljj <92416661+dunowljj@users.noreply.github.com>
2025-11-11 01:54:02 +0900

    [Gold IV] Title: 최단경로, Time: 648 ms, Memory: 108332 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,46 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-
-import static java.util.Comparator.*;
 
 public class Main {
 
-    public static final String NEW_LINE = "\n";
     public static final String INF = "INF";
+    static List<Node>[] adjList;
+    static int[] dist;
 
-    static class Node {
-        int no;
-        int distance;
+    static int V,E,start;
 
-        public Node(int no, int distance) {
-            this.no = no;
-            this.distance = distance;
-        }
-
-        public int getDistance() {
-            return distance;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int start = Integer.parseInt(br.readLine());
-
-        int[] distances = new int[V + 1];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[start] = 0;
-
-        boolean[] visited = new boolean[V + 1];
-
-        List<Node>[] graph = new ArrayList[V + 1];
-        for (int i = 0; i < V + 1; i++) {
-            graph[i] = new ArrayList<>();
+        adjList = new ArrayList[V + 1];
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
         }
+
+        dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
@@ -49,37 +34,59 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            graph[u].add(new Node(v, w));
+            adjList[u].add(new Node(v, w));
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(comparingInt(Node::getDistance));
+
+        daijkstra();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == Integer.MAX_VALUE) sb.append(INF);
+            else sb.append(dist[i]);
+
+            sb.append(System.lineSeparator());
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void daijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[V + 1];
         pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            
-            if (now.distance > distances[now.no]) continue;
+            Node node = pq.poll();
+            int cur = node.end;
+            int curWeight = node.weight;
 
-            visited[now.no] = true;
+            if (visited[cur]) continue;
+            visited[cur] = true;
 
-            for (Node next : graph[now.no]) {
-                if (!visited[next.no] && distances[next.no] > distances[now.no] + next.distance) {
-                    distances[next.no] = distances[now.no] + next.distance;
-                    pq.offer(new Node(next.no, distances[next.no]));
+            for (Node next : adjList[cur]) {
+                if (dist[next.end] > curWeight + next.weight) {
+                    dist[next.end] = curWeight + next.weight;
+                    pq.offer(new Node(next.end, dist[next.end]));
                 }
             }
         }
+    }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < distances.length; i++) { //0번 인덱스 제거 기억하기
-            if (distances[i] == Integer.MAX_VALUE) {
-                sb.append(INF).append(NEW_LINE);
-            } else {
-                sb.append(distances[i]).append(NEW_LINE);
-            }
+    static class Node implements Comparable<Node> {
+
+        int end;
+        int weight;
+
+        public Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
     }
 }
