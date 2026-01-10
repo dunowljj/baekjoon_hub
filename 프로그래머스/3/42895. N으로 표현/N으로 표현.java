d395bf1be8d375725a3cd1b5be1fdde70693aba2
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-01-10 10:20:11 +0900

    [level 3] Title: N으로 표현, Time: 5.80 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,52 +1,47 @@
 import java.util.*;
 
 class Solution {
+    final int[] numbers = new int[9];
+    
     public int solution(int N, int number) {
-        Map<Integer, Set<Integer>> dp = new HashMap<>(); // <사용 횟수, 결과들>
+        Set<Integer>[] set = new HashSet[9];
+        for (int i = 0; i < 9 ; i++) set[i] = new HashSet<>();
         
-        for (int i = 1; i <= 8; i++) {
-           dp.put(i, new HashSet<>());
-        }
-        
-        for (int i = 1; i <= 8; i++) {
-            int repeatNum = repeat(N, i);
-            dp.get(i).add(repeatNum);
+        // n[1] = 5, n[2] = 55...
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                numbers[i] += N * Math.pow(10, j);
+            }
             
-            for (int j = 1; j < i; j++) {
-                for (int a : dp.get(j)) {
-                    for (int b : dp.get(i - j)) {
-                        dp.get(i).add(a + b);
-                        dp.get(i).add(a * b);
-                        dp.get(i).add(a - b);
-                        
-                        if (b != 0) dp.get(i).add(a / b);
-                    }
+            set[i].add(numbers[i]);
+        }
+       
+        for (int i = 1; i < 9; i++) {
+            for (int operand1 : set[i]) {
+                for (int j = 1; j + i < 9; j++) {
+                    int operand2 = numbers[j];
+                    
+                    set[i + j].add(operand1 + operand2);
+                    set[i + j].add(operand1 * operand2);
+                    set[i + j].add(operand1 - operand2);
+                    set[i + j].add(operand2 - operand1);
+                    if (operand1 != 0) set[i + j].add(operand2 / operand1);
+                    if (operand2 != 0) set[i + j].add(operand1 / operand2);
                 }
             }
             
-            if (dp.get(i).contains(number)) {
-                return i;
-            }
+            if (set[i].contains(number)) return i;
         }
         
         return -1;
     }
-    
-    private int repeat(int N, int i) {
-        int result = 0;
-        
-        while(i-- > 0) {
-            result += N * Math.pow(10, i);
-        }
-        
-        return result;
-    }
 }
+
 /**
 
-11 111 1111 11111
 
-9 7개-> 999만 *9 = 8~9천만 -> int 범위 이내
+55
++/*-
+8이 최대 최솟값
 
-N을 i번
-*/
+**/
