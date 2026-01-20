9004105ea50f4108423646ee7ce8ccf5157fa2ed
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-01-20 11:16:58 +0900

    [Gold V] Title: 극장 좌석, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Gold/2302. 극장 좌석/극장 좌석.java
+++ 백준/Gold/2302. 극장 좌석/극장 좌석.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 public class Main {
 
     static boolean[] isVIP;
+    static int[] dp;
     static int N, M;
-    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,32 +15,23 @@ public class Main {
         M = Integer.parseInt(br.readLine());
 
         isVIP = new boolean[N + 1];
+        dp = new int[N + 1];
 
         for (int i = 0; i < M; i++) {
             int fixNo = Integer.parseInt(br.readLine());
             isVIP[fixNo] = true;
         }
 
-        dfs(1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < N + 1; i++) {
+            dp[i] = dp[i - 1];
 
-        System.out.println(count);
-    }
-
-    static void dfs(int idx) {
-        if (idx >= N) {
-            count++;
-            return;
+            if (!(isVIP[i - 1] || isVIP[i])) {
+                dp[i] += dp[i - 2];
+            }
         }
 
-        if (!isVIP[idx] && !isVIP[idx + 1])  {
-            dfs(idx + 2);
-        }
-
-        dfs(idx + 1);
+        System.out.print(dp[N]);
     }
 }
-/**
- * - vip아니라면 swap 가능.
- * - swap한 곳을 다시 swap하면 인접이 아님
- * - swap하지 않고 넘어갈 수도 있음
- */
