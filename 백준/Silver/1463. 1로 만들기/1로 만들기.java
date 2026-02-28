4a7dc8d764448f950c5119cc531df1367ad29db7
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-02-28 14:31:45 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,35 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Integer[] dp = new Integer[N + 1];
-        dp[N] = 0;
+        int[] arr = new int[N + 1];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        arr[N] = 0;
 
-        for (int i = N; i >= 1; i--) {
-            if (dp[i] == null) continue;
+        for (int i = N; i > 0; i--) {
+            int count = arr[i] + 1;
 
-            if (i % 3 == 0) {
-                if (dp[i / 3] == null || dp[i / 3] > dp[i] + 1) {
-                    dp[i / 3] = dp[i] + 1;
-                }
-            }
-
-            if (i % 2 == 0) {
-                if (dp[i / 2] == null || dp[i / 2] > dp[i] + 1) {
-                    dp[i / 2] = dp[i] + 1;
-                }
-            }
-
-            if (dp[i - 1] == null || dp[i - 1] > dp[i] + 1) {
-                dp[i - 1] = dp[i] + 1;
-            }
+            if (i % 3 == 0) arr[i / 3] = Math.min(arr[i / 3], count);
+            if (i % 2 == 0) arr[i / 2] = Math.min(arr[i / 2], count);
+            arr[i - 1] = Math.min(arr[i - 1], count);
         }
 
-        System.out.print(dp[1]);
+        System.out.print(arr[1]);
     }
 }
