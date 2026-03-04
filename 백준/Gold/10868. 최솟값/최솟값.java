bfa1c2e14bd296f9191c7253b4a59aec91122f9d
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-03-04 18:41:09 +0900

    [Gold I] Title: 최솟값, Time: 500 ms, Memory: 58500 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.java
+++ 백준/Gold/10868. 최솟값/최솟값.java
@@ -4,62 +4,55 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[][] dp;
+    static int[] log;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int K = (int)(Math.log(N) / Math.log(2)) + 1;
 
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][K];
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = Integer.parseInt(br.readLine());
         }
 
-        MinSegmentTree root = new MinSegmentTree();
-        build(arr, root, 0, arr.length - 1);
+        log = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            log[i] = log[i/2] + 1;
+        }
+
+        for (int k = 1; k < K; k++) {
+            int size = (1 << k);
+            for (int n = 1; n + size <= N + 1; n++) {
+                dp[n][k] = Math.min(dp[n][k - 1], dp[n+(size/2)][k - 1]);
+            }
+        }
+
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            sb.append(query(root, 0, arr.length - 1, a, b))
+            int k = log[b - a + 1];
+
+            sb.append(Math.min(
+                            dp[a][k],
+                            dp[b - (1<<k) + 1][k])
+                    )
                     .append(System.lineSeparator());
         }
 
         System.out.print(sb.toString().trim());
     }
-
-    private static int build(int[] arr, MinSegmentTree node, int start, int end) {
-        if (start == end) {
-            return node.min = arr[start];
-        }
-
-        int mid = (start + end) / 2;
-        return node.min = Math.min(
-                build(arr, node.left = new MinSegmentTree(), start, mid),
-                build(arr, node.right = new MinSegmentTree(), mid + 1, end)
-        );
-    }
-
-    private static int query(MinSegmentTree node, int start, int end, int l, int r) {
-        if (r < start || end < l) return Integer.MAX_VALUE;
-        if (l <= start && end <= r) return node.min;
-
-        int mid = (start + end) / 2;
-        return Math.min(
-                query(node.left, start, mid, l, r),
-                query(node.right, mid + 1, end, l, r)
-        );
-    }
-
-    public static class MinSegmentTree {
-        int min;
-        MinSegmentTree left;
-        MinSegmentTree right;
-
-        public MinSegmentTree() {}
-    }
 }
+/**
+ * sparse tree 형태
+ * 겹치는 부분은 min 연산이라 상관없다.
+ */
