2eba4469bc6954069006a25a225eb0b2250ebd34
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-03-16 23:03:31 +0900

    [Gold V] Title: 암호코드, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.java
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -15,7 +15,7 @@ public class Main {
 
         password = br.readLine();
         n = password.length();
-        dp = new int[n];
+        dp = new int[n + 1];
 
         // 시작이 0인 경우 불가능
         if (password.charAt(0) == '0') {
@@ -29,45 +29,23 @@ public class Main {
             return;
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            int val1 = password.charAt(i) - '0';
-            int val2 = password.charAt(i + 1) - '0';
-
-            // '0' 2개 연속 -> 불가능
-            if (val1 == 0 && val2 == 0) {
-                System.out.print(0);
-                return;
-            }
-
-            // '0' 앞에 1,2가 없음 -> 불가능
-            if (val1 > 2 && val2 == 0) {
-                System.out.print(0);
-                return;
-            }
-        }
-
         dp[0] = 1;
-        int v1 = password.charAt(0) - '0';
-        int v2 = password.charAt(1) - '0';
-        if (v1 == 1 || (v1 == 2 && v2 <= 6)) dp[1] = 1;
+        dp[1] = 1;
 
-        for (int i = 1; i < n; i++) {
-            int val = password.charAt(i) - '0';
+        for (int i = 2; i <= n; i++) {
+            int curr = password.charAt(i - 1) - '0';
+            int prev = password.charAt(i - 2) - '0';
 
-            if (val != 0) dp[i] += dp[i - 1];
+            if (curr != 0) dp[i] += dp[i - 1];
 
-            if (i < n - 1) {
-                int nextVal = password.charAt(i + 1) - '0';
-
-                if (val == 1 || (val == 2 && nextVal <= 6)) {
-                    dp[i + 1] += dp[i - 1];
-                }
+            if (prev == 1 || (prev == 2 && curr <= 6)) {
+                dp[i] += dp[i - 2];
             }
 
             dp[i] %= MOD;
         }
 
-        System.out.print(dp[n - 1]);
+        System.out.print(dp[n]);
     }
 }
 
